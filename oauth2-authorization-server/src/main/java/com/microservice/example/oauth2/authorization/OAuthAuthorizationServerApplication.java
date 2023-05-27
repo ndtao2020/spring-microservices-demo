75f0ea183781619292c81cfb7ee9a82025f3b363
Nguyễn Đình Tạo <ndtao2020@gmail.com>
2023-05-27 09:01:06 +0700

    add ..

---
+++ oauth2-authorization-server/src/main/java/com/microservice/example/oauth2/authorization/OAuthAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.example.oauth2.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthAuthorizationServerApplication.class, args);
+	}
+
+}
