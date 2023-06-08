b16588258c3bf4c181ce08517a87a7d809089bf3
Nguyễn Đình Tạo <ndtao2020@gmail.com>
2023-06-08 11:36:26 +0700

    Simple HTTP Request in Java

---
+++ src/jmh/java/com/microservice/benchmark/http/GetMethod.java
@@ -0,0 +1,101 @@
+package com.microservice.benchmark.http;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import jodd.http.HttpRequest;
+import jodd.http.HttpResponse;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.routing.RoutingSupport;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpException;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.io.HttpClientResponseHandler;
+import org.eclipse.jetty.client.ContentResponse;
+import org.eclipse.jetty.client.HttpClient;
+import org.openjdk.jmh.annotations.*;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class GetMethod {
+
+    public static final String API_URL = "https://jsonplaceholder.typicode.com/posts";
+
+    @Benchmark
+    public JSONArray java() throws IOException {
+        final URL url = new URL(API_URL);
+        final HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        try (final InputStream is = con.getInputStream()) {
+            return JSON.parseArray(is);
+        }
+    }
+
+    @Benchmark
+    public JSONArray apacheHttp() throws IOException {
+        HttpGet httpGet = new HttpGet(API_URL);
+        try (final CloseableHttpClient httpclient = HttpClients.createDefault();
+             final CloseableHttpResponse response = httpclient.execute(httpGet);
+             final HttpEntity entity = response.getEntity();
+             final InputStream is = entity.getContent()) {
+            return JSON.parseArray(is);
+        }
+    }
+
+    @Benchmark
+    public JSONArray apacheHttpHandle() throws IOException, HttpException {
+        HttpGet httpGet = new HttpGet(API_URL);
+        HttpHost httpHost = RoutingSupport.determineHost(httpGet);
+        HttpClientResponseHandler<JSONArray> responseHandler = response -> {
+            try (final HttpEntity entity = response.getEntity();
+                 final InputStream is = entity.getContent()) {
+                return JSON.parseArray(is);
+            }
+        };
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            return httpclient.execute(httpHost, httpGet, null, responseHandler);
+        }
+    }
+
+    @Benchmark
+    public JSONArray okhttp() throws IOException {
+        final OkHttpClient client = new OkHttpClient();
+        final Request request = new Request.Builder().url(API_URL).build();
+        try (Response response = client.newCall(request).execute(); ResponseBody responseBody = response.body()) {
+            return JSON.parseArray(responseBody.bytes());
+        }
+    }
+
+    @Benchmark
+    public JSONArray jetty() throws Exception {
+        final HttpClient client = new HttpClient();
+        client.start();
+        JSONArray jsonArray;
+        try {
+            final ContentResponse res = client.GET(API_URL);
+            jsonArray = JSON.parseArray(res.getContent());
+        } finally {
+            client.stop();
+        }
+        return jsonArray;
+    }
+
+    @Benchmark
+    public JSONArray jodd() {
+        final HttpResponse response = HttpRequest.get(API_URL).send();
+        return JSON.parseArray(response.bodyBytes());
+    }
+}
---
+++ src/test/java/com/microservice/example/http/GetMethodTests.java
@@ -0,0 +1,131 @@
+package com.microservice.example.http;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import jodd.http.HttpRequest;
+import jodd.http.HttpResponse;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.routing.RoutingSupport;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpException;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.io.HttpClientResponseHandler;
+import org.eclipse.jetty.client.ContentResponse;
+import org.eclipse.jetty.client.HttpClient;
+import org.junit.jupiter.api.Test;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class GetMethodTests {
+
+    public static final String API_URL = "https://jsonplaceholder.typicode.com/posts";
+
+    @Test
+    void java() throws IOException {
+        URL url = new URL(API_URL);
+        HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        try (final InputStream is = con.getInputStream()) {
+            JSONArray jsonArray = JSON.parseArray(is);
+            assertNotNull(jsonArray);
+            for (Object obj : jsonArray) {
+                JSONObject jsonObject = (JSONObject) obj;
+                assertEquals(Integer.class, jsonObject.get("userId").getClass());
+            }
+        }
+    }
+
+    @Test
+    void apacheHttpClient() throws IOException {
+        HttpGet httpGet = new HttpGet(API_URL);
+        try (CloseableHttpClient httpclient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpclient.execute(httpGet);
+             HttpEntity entity = response.getEntity()) {
+            try (final InputStream is = entity.getContent()) {
+                JSONArray jsonArray = JSON.parseArray(is);
+                assertNotNull(jsonArray);
+                for (Object obj : jsonArray) {
+                    JSONObject jsonObject = (JSONObject) obj;
+                    assertEquals(Integer.class, jsonObject.get("userId").getClass());
+                }
+            }
+        }
+    }
+
+    @Test
+    void apacheHttpClientHandle() throws IOException, HttpException {
+        HttpGet httpGet = new HttpGet(API_URL);
+        HttpHost httpHost = RoutingSupport.determineHost(httpGet);
+        HttpClientResponseHandler<JSONArray> responseHandler = response -> {
+            final HttpEntity entity = response.getEntity();
+            try (final InputStream is = entity.getContent()) {
+                return JSON.parseArray(is);
+            }
+        };
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            JSONArray jsonArray = httpclient.execute(httpHost, httpGet, null, responseHandler);
+            assertNotNull(jsonArray);
+            for (Object obj : jsonArray) {
+                JSONObject jsonObject = (JSONObject) obj;
+                assertEquals(Integer.class, jsonObject.get("userId").getClass());
+            }
+        }
+    }
+
+    @Test
+    void okhttp() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(API_URL).build();
+        try (Response response = client.newCall(request).execute();
+             ResponseBody responseBody = response.body()) {
+            JSONArray jsonArray = JSON.parseArray(responseBody.bytes());
+            assertNotNull(jsonArray);
+            for (Object obj : jsonArray) {
+                JSONObject jsonObject = (JSONObject) obj;
+                assertEquals(Integer.class, jsonObject.get("userId").getClass());
+            }
+        }
+    }
+
+    @Test
+    void jetty() throws Exception {
+        HttpClient client = new HttpClient();
+        client.start();
+        try {
+            ContentResponse res = client.GET(API_URL);
+            JSONArray jsonArray = JSON.parseArray(res.getContent());
+            assertNotNull(jsonArray);
+            for (Object obj : jsonArray) {
+                JSONObject jsonObject = (JSONObject) obj;
+                assertEquals(Integer.class, jsonObject.get("userId").getClass());
+            }
+        } finally {
+            client.stop();
+        }
+    }
+
+    @Test
+    void jodd() {
+        HttpResponse response = HttpRequest.get(API_URL).send();
+        JSONArray jsonArray = JSON.parseArray(response.bodyBytes());
+        assertNotNull(jsonArray);
+        for (Object obj : jsonArray) {
+            JSONObject jsonObject = (JSONObject) obj;
+            assertEquals(Integer.class, jsonObject.get("userId").getClass());
+        }
+    }
+}
