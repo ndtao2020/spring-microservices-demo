ef77ca50e2802f145df1c91e2f940ece60d72955
Nguyễn Đình Tạo <75662783+ndtao2020@users.noreply.github.com>
2023-12-16 14:28:02 +0700

    123

--- spring-admin-service/src/main/java/com/microservice/example/SecurityConfig.java
+++ spring-admin-service/src/main/java/com/microservice/example/SecurityConfig.java
@@ -1,21 +1,30 @@
 package com.microservice.example;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+                // make sure we use stateless session; session won't be used to store user's state.
+                .sessionManagement(configure -> configure
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // handle an authorized attempts
+                .exceptionHandling(configure -> configure
+                        .authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
+                .authorizeHttpRequests(configure -> configure
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 }
