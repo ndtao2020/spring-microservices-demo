7da765671aeed41b9cd321065eb4d58dfe6d4eee
Nguyễn Đình Tạo <75662783+ndtao2020@users.noreply.github.com>
2024-07-27 21:30:31 +0700

    v
    Took 1 hour 11 minutes

--- src/main/java/com/microservice/example/jwt/Payload.java
+++ src/main/java/com/microservice/example/jwt/Payload.java
@@ -2,8 +2,15 @@ package com.microservice.example.jwt;
 
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
-public class Payload {
+public class Payload implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = -16481L;
+
   private String iss;
   private String sub;
   private String aud;
